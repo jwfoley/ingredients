9e3b27c430ec11b681a03c0625e0813c5e8b7023
Joe Foley <joe@jwfoley.com>
2022-04-05 17:34:07 -0700

    refactor for new format

--- ingredients/ingredients.py
+++ ingredients/ingredients.py
@@ -1,18 +1,13 @@
 import re, markdown
 from collections import defaultdict
 from dataclasses import dataclass
-from warnings import warn
 from lxml import etree, html
 
-DIRECTION_REGEX = '^\* \[ \] ' # matches GFM "* [ ] "
+# output formats
 DIRECTIONS_FORMAT = '<label><input type="checkbox">%s</label><p>'
-
-INGREDIENTS_TAG = 'ingredients' # appears as an HTML tag
 INGREDIENTS_STYLE = 'margin-left: 3em' # style hardcoded to each ingredients table, currently set to indent it nicely
-SCALE_TAG = 'scale' # appears as an HTML tag
-DEFAULT_SCALE_NAME = 'batches'
-
-DEFAULT_HEADER = ['''<meta name="viewport" content="initial-scale=1">
+DEFAULT_SCALE_LABEL = 'batches'
+DEFAULT_HEADER = '''<meta name="viewport" content="initial-scale=1">
 <style>
 input[type=checkbox] {
 	transform: scale(1.5);
@@ -24,7 +19,21 @@ input[type="text"] {
 input[type="number"] {
 	width: 4em;
 }
-</style>'''] # think harder about where to put this
+</style>''' # think harder about where to put this
+
+# input formats
+DIRECTION_REGEX = '^\* \[ \] ' # matches GFM "* [ ] "
+INGREDIENTS_REGEX = re.compile('```\{ingredients,?(.*?)\}(.*?)```', re.DOTALL) # returns the options and the table as groups
+SCALE_REGEX = re.compile('<!scale,?(.*?)>', re.DOTALL)
+
+
+def parse_opts (text):
+	'''
+	parse a simple batch of options in the form "option1 = value, option2 = value" with spaces optional
+	return a dictionary; keys and values will all be strings
+	simple hack: make it resemble a dummy HTML tag and use the HTML parser!
+	'''
+	return dict(html.fromstring('<dummy,' + text + '>').items())
 
 class Directions (markdown.preprocessors.Preprocessor):
 	def run (self, lines):
@@ -50,17 +59,17 @@ class Directions (markdown.preprocessors.Preprocessor):
 		return new_lines
 
 @dataclass
-class IngredientsTable:
+class IngredientTable:
 	ingredients: list
 	total: float = 0
 
-def parse_ingredients_table (text):
+def parse_ingredient_table (text):
 	ingredients = []
 	has_consistent_unit = True # assume yes until proven no
 	consistent_unit = None
 	total = 0
 	for line in text.split('\n'): 
-		stripped_line = line.strip()
+		stripped_line = line.strip('\s\0')
 		if stripped_line != '':
 			fields = re.split('\s*\|\s*', stripped_line)
 			ingredient = fields[0]
@@ -77,50 +86,33 @@ def parse_ingredients_table (text):
 					has_consistent_unit = False
 					total = None
 	
-	return(IngredientsTable(ingredients, total))
+	return(IngredientTable(ingredients, total))
 
 
-def generate_ingredient_table (element, form_name = None, scale_name = None, default_scale = None, checkbox = True):
+def format_ingredient_table (
+	table,
+	title = None,
+	name = None,
+	scale = None,
+	scale_label = DEFAULT_SCALE_LABEL,
+	default_scale = 1,
+	checkbox = True
+):
 	'''
-	given an lxml.etree.Element of tag 'ingredients', return a new Element in the form of an HTML form containing the interactive table
+	given an IngredientTable, return an HTML form containing the interactive table
 	'''
-	has_standalone_scale = 'scale' in element.keys()
-	
-	# get arguments from input (unless overridden by runtime arguments)
-	if form_name is None:
-		if 'form_name' in element.keys():
-			form_name = element.get('form_name')
-		else:
-			form_name = 'ingredients'
-	
-	if scale_name is None:
-		if 'scale_name' in element.keys():
-			if has_standalone_scale: warn('"scale_name" is given for an ingredients table that already has a "scale" and is therefore ignored')
-			scale_name = element.get('scale_name')
-		else:
-			scale_name = DEFAULT_SCALE_NAME
-	
-	if default_scale is None:
-		if 'default_scale' in element.keys():
-			if has_standalone_scale: warn('"default_scale" is given for an ingredients table that already has a "scale" and is therefore ignored')
-			default_scale = element.get('default_scale')
-		else:
-			default_scale = '1'
-
-	# parse the ingredients
-	table = parse_ingredients_table(element.text)
 	
 	# create the new Element
 	form_root = etree.Element('form', {
-		'name': form_name,
+		'name': name,
 		'style': INGREDIENTS_STYLE
 	})
-	if 'title' in element.keys():
+	if title is not None:
 		form_title = etree.SubElement(form_root, 'h4')
-		form_title.text = element.get('title')
-		if not has_standalone_scale: form_title.tail = scale_name + ': '
+		form_title.text = title
+		if scale is None: form_title.tail = scale_label + ': '
 	else:
-		if not has_standalone_scale: form_root.text = scale_name + ': '
+		if scale is None: form_root.text = scale_label + ': '
 	
 	# hardcoded default values
 	for i in range(len(table.ingredients)):
@@ -137,7 +129,7 @@ def generate_ingredient_table (element, form_name = None, scale_name = None, def
 		}))
 	
 	# scale controls
-	if not has_standalone_scale:
+	if scale is None:
 		scale_function = etree.SubElement(form_root, 'input', {
 			'type': 'number',
 			'name': 'scale',
@@ -145,9 +137,9 @@ def generate_ingredient_table (element, form_name = None, scale_name = None, def
 			'onInput': ''
 		})
 		for i in range(len(table.ingredients)):
-			scale_function.set('onInput', scale_function.get('onInput') + ('document.%s.amount%i.value = document.%s.scale.value * document.%s.default%i.value;' % (form_name, i, form_name, form_name, i)))
+			scale_function.set('onInput', scale_function.get('onInput') + ('document.%s.amount%i.value = document.%s.scale.value * document.%s.default%i.value;' % (name, i, name, name, i)))
 		if table.total is not None:
-			scale_function.set('onInput', scale_function.get('onInput') + ('document.%s.total.value = document.%s.scale.value * document.%s.default_total.value;' % (form_name, form_name, form_name)))
+			scale_function.set('onInput', scale_function.get('onInput') + ('document.%s.total.value = document.%s.scale.value * document.%s.default_total.value;' % (name, name, name)))
 		
 		# reset button
 		form_root.append(etree.Element('input', {'type': 'reset', 'value': 'Reset'}))
@@ -185,26 +177,15 @@ def generate_ingredient_table (element, form_name = None, scale_name = None, def
 		})
 		total_box.tail = ' ' + table.ingredients[0][2] # unit, assuming all consistent because the total was given
 	
-	return form_root
+	return html.tostring(form_root).decode('utf-8')
 
-def generate_scale(element, ingredients_tables, form_name, scale_name = None, default_scale = None):
+def generate_scale(ingredients_tables, name, scale_label = DEFAULT_SCALE_LABEL, default_scale = 1):
 	'''
-	given an lxml.etree.Element of tag 'scale' and a list of ingredient table names and lengths, return a new Element in the form of an HTML form containing the interactive scale adjuster
+	given a list of ingredient table names and lengths, return an HTML form containing the interactive scale adjuster
 	'''
-	if scale_name is None:
-		if 'scale_name' in element.keys():
-			scale_name = element.get('scale_name')
-		else:
-			scale_name = DEFAULT_SCALE_NAME
 	
-	if default_scale is None:
-		if 'default_scale' in element.keys():
-			default_scale = element.get('default_scale')
-		else:
-			default_scale = '1'
-	
-	form_root = etree.Element('form', {'name': form_name})
-	form_root.text = scale_name + ': '
+	form_root = etree.Element('form', {'name': name})
+	form_root.text = scale_label + ': '
 	scale_function = etree.SubElement(form_root, 'input', {
 		'type': 'number',
 		'name': 'scale',
@@ -215,50 +196,48 @@ def generate_scale(element, ingredients_tables, form_name, scale_name = None, de
 	for ingredients_name, ingredient_count, has_common_unit in ingredients_tables:
 		reset_button.set('onClick', reset_button.get('onClick') + ('document.%s.reset();' % ingredients_name))
 		for i in range(ingredient_count):
-			scale_function.set('onInput', scale_function.get('onInput') + ('document.%s.amount%i.value = document.%s.scale.value * document.%s.default%i.value;' % (ingredients_name, i, form_name, ingredients_name, i)))
+			scale_function.set('onInput', scale_function.get('onInput') + ('document.%s.amount%i.value = document.%s.scale.value * document.%s.default%i.value;' % (ingredients_name, i, name, ingredients_name, i)))
 		if has_common_unit:
-			scale_function.set('onInput', scale_function.get('onInput') + ('document.%s.total.value = document.%s.scale.value * document.%s.default_total.value;' % (ingredients_name, form_name, ingredients_name)))
+			scale_function.set('onInput', scale_function.get('onInput') + ('document.%s.total.value = document.%s.scale.value * document.%s.default_total.value;' % (ingredients_name, name, ingredients_name)))
 	
-	return form_root
+	return html.tostring(form_root).decode('utf-8')
 
 
 class Ingredients (markdown.preprocessors.Preprocessor):
+	def __init__ (self):
+		self.scale_tables = defaultdict(list) # each key is the name of a standalone scale and its value is a list of tuples: each tuple contains the name of an ingredients table, the number of ingredients in it, and whether it includes a total
+		self.ingredients_counter = 0
+	
+	def generate_ingredient_tables (self, match):
+		'''
+		generate an ingredients table, but name it uniquely after the counter (then increment that) and note if there's a standalone scale that should point to it
+		'''
+		opts = parse_opts(match.groups()[0])
+		parsed_table = parse_ingredient_table(match.groups()[1])
+		table_name = 'ingredients%i' % self.ingredients_counter
+		if 'scale' in opts:
+			self.scale_tables[opts['scale']].append((
+				table_name,
+				len(parsed_table.ingredients), # table ingredients
+				parsed_table.total is not None # whether table has a total
+			))
+		self.ingredients_counter += 1
+		return format_ingredient_table(parsed_table, name = table_name, **opts)
+	
+	def generate_scales (self, match):
+		opts = parse_opts(match.groups()[0])
+		return generate_scale(self.scale_tables[opts['name']], **opts)
+	
 	def run (self, lines):
-		parsed_html = html.fromstring('\n'.join(lines))
+		textwall = '\n'.join(lines)
 		
-		# first pass: enumerate the standalone scales
-		ingredients_counter_first_pass = 0
-		scale_tables = defaultdict(list) # each key is the name of a standalone scale and its value is a list of tuples: each tuple contains the name of an ingredients table and the number of ingredients in it
-		for i in range(len(parsed_html)):
-			if parsed_html[i].tag == INGREDIENTS_TAG:
-				if 'scale' in parsed_html[i].keys(): # ingredients table linked to standalone scale
-					scale_name = parsed_html[i].get('scale')
-					table = parse_ingredients_table(parsed_html[i].text)
-					scale_tables[scale_name].append((
-						'ingredients%i' % ingredients_counter_first_pass, # table name
-						len(table.ingredients), # table length
-						table.total is not None # whether table has a total
-					))
-					
-				ingredients_counter_first_pass +=1 
+		# first pass: generate tables and enumerate the standalone scales
+		textwall = INGREDIENTS_REGEX.sub(self.generate_ingredient_tables, textwall)
 		
-		# second pass: create the forms
-		ingredients_counter_second_pass = 0
-		for i in range(len(parsed_html)):
-			if parsed_html[i].tag == INGREDIENTS_TAG:
-				new_element = generate_ingredient_table(parsed_html[i], ('ingredients%i' % ingredients_counter_second_pass))
-				new_element.tail = parsed_html[i].tail
-				parsed_html[i] = new_element
-				ingredients_counter_second_pass += 1
-			elif parsed_html[i].tag == SCALE_TAG:
-				scale_name = parsed_html[i].get('name')
-				new_element = generate_scale(parsed_html[i], scale_tables[scale_name], scale_name)
-				new_element.tail = parsed_html[i].tail
-				parsed_html[i] = new_element
+		# second pass: generate standalone scales
+		textwall = SCALE_REGEX.sub(self.generate_scales, textwall)
 		
-		assert(ingredients_counter_second_pass == ingredients_counter_first_pass)
-		
-		return DEFAULT_HEADER + html.tostring(parsed_html)[3:-4].decode('utf-8').split('\n') # the [3:-4] is a workaround to remove the unwanted <p> tags added for the root tree
+		return (DEFAULT_HEADER + textwall).splitlines()
 
 
 class IngredientExtension (markdown.extensions.Extension):
