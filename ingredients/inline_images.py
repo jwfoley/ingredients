2ccb4fa4172171e2e8a8da80f8ba47c16c0dd9dd
Joe Foley <joe@jwfoley.com>
2022-07-05 14:50:16 -0700

    allow multiple inline images per line

--- ingredients/inline_images.py
+++ ingredients/inline_images.py
@@ -5,11 +5,11 @@ from textwrap import wrap
 WRAP_LENGTH = 80 # length of wrapped lines with raw image data
 IMAGE_REGEX = re.compile('!\[(.*?)\]\((.*?)\)\{(.*?)\}', re.DOTALL)
 TAG_REGEX = re.compile('\.inline\s*') # include trailing space to delete it when passing style
-IMAGE_FORMAT = '%s<img%s src="data:image;base64,\n%s\n">%s'
+IMAGE_FORMAT = '<img%s src="data:image;base64,\n%s\n">'
 SVG_REGEX = re.compile(b'<svg(.*?)>(.+)</svg>', re.DOTALL) # binary to identify raw data before decoding
 TAG_SIZE_REGEX = re.compile('[^ ]*(width|height)=[^ ]+ *')
 STYLE_SIZE_REGEX = re.compile('(width|height):')
-SVG_FORMAT = '%s<svg%s>%s</svg>%s'
+SVG_FORMAT = '<svg%s>%s</svg>'
 
 class InlineImages (markdown.preprocessors.Preprocessor):
 	def run (self, lines):
@@ -17,43 +17,39 @@ class InlineImages (markdown.preprocessors.Preprocessor):
 	
 	def insert_images (self, lines):
 		for line in lines:
-			line_match = IMAGE_REGEX.search(line)
-			if line_match is None:
-				yield line
-				continue
-			
-			style_sub = TAG_REGEX.subn('', line_match.group(3))
-			if style_sub[1] != 1: # found tag once (and removed it)
-				yield line
-				continue
-			
-			raw_image = open(line_match.group(2), 'rb').read()
-			line_before = line[:line_match.start()]
-			line_after = line[line_match.end():]
-			style = style_sub[0].strip()
-			style_attrib = '' if style == '' else (' style="%s"' % style)
-			title = line_match.group(1).strip()
-			
-			# SVG
-			svg_match = SVG_REGEX.search(raw_image)
-			if svg_match is not None:
-				# remove previous height and width if (re)specified in style
-				svg_tag = svg_match.group(1).decode()
-				if (STYLE_SIZE_REGEX.search(style) is not None): svg_tag = TAG_SIZE_REGEX.sub('', svg_tag)
+			for image_match in list(IMAGE_REGEX.finditer(line))[::-1]: # iterate in reverse so coordinates from left will still be correct after each replacement
+				style_sub = TAG_REGEX.subn('', image_match.group(3))
+				if style_sub[1] != 1: # found tag once (and removed it)
+					yield line
+					continue
 				
-				yield SVG_FORMAT % (
-					line_before,
-					svg_tag + style_attrib,
-					('' if title == '' else ('<title>%s</title>' % title)) + svg_match.group(2).decode(),
-					line_after
-				)
+				raw_image = open(image_match.group(2), 'rb').read()
+				line_before = line[:image_match.start()]
+				line_after = line[image_match.end():]
+				style = style_sub[0].strip()
+				style_attrib = '' if style == '' else (' style="%s"' % style)
+				title = image_match.group(1).strip()
+				
+				# SVG
+				svg_match = SVG_REGEX.search(raw_image)
+				if svg_match is not None:
+					# remove previous height and width if (re)specified in style
+					svg_tag = svg_match.group(1).decode()
+					if (STYLE_SIZE_REGEX.search(style) is not None): svg_tag = TAG_SIZE_REGEX.sub('', svg_tag)
+					
+					replacement = SVG_FORMAT % (
+						svg_tag + style_attrib,
+						('' if title == '' else ('<title>%s</title>' % title)) + svg_match.group(2).decode()
+					)
+				
+				# not SVG
+				else:
+					replacement = IMAGE_FORMAT % (
+						style_attrib + ('' if title == '' else (' title="%s"' % title)),
+						'\n'.join(wrap(b64encode(raw_image).decode(), WRAP_LENGTH))
+					)
+				
+				line = line[:image_match.start()] + replacement + line[image_match.end():]
 			
-			# not SVG
-			else:
-				yield IMAGE_FORMAT % (
-					line_before,
-					style_attrib + ('' if title == '' else (' title="%s"' % title)),
-					'\n'.join(wrap(b64encode(raw_image).decode(), WRAP_LENGTH)),
-					line_after
-				)
+			yield line
 
