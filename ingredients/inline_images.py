017e5bd61e9f04d35b731f1e76d00f66ad521afe
Joe Foley <joe@jwfoley.com>
2022-07-05 14:27:03 -0700

    override SVG size if specified by style

--- ingredients/inline_images.py
+++ ingredients/inline_images.py
@@ -6,7 +6,9 @@ WRAP_LENGTH = 80 # length of wrapped lines with raw image data
 IMAGE_REGEX = re.compile('!\[(.*?)\]\((.*?)\)\{(.*?)\}', re.DOTALL)
 TAG_REGEX = re.compile('\.inline\s*') # include trailing space to delete it when passing style
 IMAGE_FORMAT = '%s<img%s src="data:image;base64,\n%s\n">%s'
-SVG_REGEX = re.compile(b'<svg(.*?)>(.+)</svg>', re.DOTALL)
+SVG_REGEX = re.compile(b'<svg(.*?)>(.+)</svg>', re.DOTALL) # binary to identify raw data before decoding
+TAG_SIZE_REGEX = re.compile('[^ ]*(width|height)=[^ ]+ *')
+STYLE_SIZE_REGEX = re.compile('(width|height):')
 SVG_FORMAT = '%s<svg%s>%s</svg>%s'
 
 class InlineImages (markdown.preprocessors.Preprocessor):
@@ -35,9 +37,13 @@ class InlineImages (markdown.preprocessors.Preprocessor):
 			# SVG
 			svg_match = SVG_REGEX.search(raw_image)
 			if svg_match is not None:
+				# remove previous height and width if (re)specified in style
+				svg_tag = svg_match.group(1).decode()
+				if (STYLE_SIZE_REGEX.search(style) is not None): svg_tag = TAG_SIZE_REGEX.sub('', svg_tag)
+				
 				yield SVG_FORMAT % (
 					line_before,
-					svg_match.group(1).decode() + style_attrib,
+					svg_tag + style_attrib,
 					('' if title == '' else ('<title>%s</title>' % title)) + svg_match.group(2).decode(),
 					line_after
 				)
